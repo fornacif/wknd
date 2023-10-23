0f9af882d2555a4e5b211146ed68e02e42d2f58f
François Fornaciari <fornacia@adobe.com>
2023-10-23 15:54:15 +0200

    Update test block

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -2,5 +2,7 @@
  * @param {HTMLElement} $block
  */
 export default function decorate(block) {
-	block.innerHTML = '<div class="customTest">test title</div>';
+	const subDiv = document.createElement('div');
+	subDiv.textContent = 'Sub Div';
+	block.appendChild(subDiv);
 }	
